09facc6fe669f16f21208e09b84e1073f295d02c
xiaofanc <xfang.chu@gmail.com>
2023-08-22 22:38:10 -0700

    add mock service

---
+++ src/services/login.js
@@ -0,0 +1,7 @@
+import { get } from '../utils/request';
+
+export const loginService = (username, password) => {
+  get(`/api/login/${username}/${password}`);
+};
+
+export const register = () => {};
